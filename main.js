20ec69cf7ff5cbe67af9a20a902e16f039ac5b15
Travis Nguyen <travisn714@gmail.com>
2018-09-03 15:49:42 -0700

    Created an array of objects representing characters in the phrase #1

---
+++ main.js
@@ -0,0 +1,35 @@
+var text = [
+  {
+    word: 'grumpy'
+  },
+  {
+    word: 'wizards'
+  },
+  {
+    word: 'make'
+  },
+  {
+    word: 'toxic'
+  },
+  {
+    word: 'brew'
+  },
+  {
+    word: 'for'
+  },
+  {
+    word: 'the'
+  },
+  {
+    word: 'evil'
+  },
+  {
+    word: 'queen'
+  },
+  {
+    word: 'and'
+  },
+  {
+    word: 'jack'
+  }
+]
