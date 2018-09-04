fd55932c1da8f5c3dbb616a8d701fb17041e7911
Travis Nguyen <travisn714@gmail.com>
2018-09-04 13:28:24 -0700

    Define a function that renders a single character #1

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-var characters = 'grumpywizardsmaketoxicbrewfortheevilqueenandjack'
+var characters = 'grumpy wizards make toxic brew for the evil queen and jack'
 
 var wordArray = []
 for (var i = 0; i < characters.length; i++) {
-  wordArray[i] = {
+  var object = {
     letter: characters[i]
   }
+  wordArray.push(object)
 }
+
 console.log(wordArray)
+
+function renderOneCharacter(character) {
+  var $oneLetter = document.createElement('span')
+  $oneLetter.textContent = character.letter
+  return $oneLetter
+  }
+
+renderOneCharacter(wordArray)
